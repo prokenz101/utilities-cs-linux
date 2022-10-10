7f3a92e26980fdec7385568d72e58c8cffabfc60
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2022-10-10 21:24:01 +0400

    icon code
    this code gets the u-cs icon from github and writes it to a file in the main folder

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@
         public static string IconURL = "https://raw.githubusercontent.com/prokenz101/utilities-cs-linux/setup/Assets/UtilitiesIcon.ico";
         static void Main(string[] args) {
             Directory.CreateDirectory(Path.Combine(UtilitiesFolderPath, "Assets")); //* Creating folders
+
+            //! Getting the icon and putting it in the Assets folder
+            var httpClient = new HttpClient();
+            var iconResponse = httpClient.GetAsync(new Uri(IconURL)).Result;
+            byte[] iconResponseOutput = iconResponse.Content.ReadAsByteArrayAsync().Result;
+            File.WriteAllBytes(IconPath, iconResponseOutput);
+            Console.WriteLine("Got the icon and wrote it to a file in Assets.\n");
         }
     }
 }
