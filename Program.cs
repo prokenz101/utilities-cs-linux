289c7fb45449e1310ed4f7667ce8f3c19394ede6
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2022-10-10 21:27:25 +0400

    main executable file
    this code gets the main executable file's project files from github, builds it and moves it to the root folder and then deletes the cloned folder

--- Program.cs
+++ Program.cs
@@ -12,6 +12,36 @@
             byte[] iconResponseOutput = iconResponse.Content.ReadAsByteArrayAsync().Result;
             File.WriteAllBytes(IconPath, iconResponseOutput);
             Console.WriteLine("Got the icon and wrote it to a file in Assets.\n");
+
+            //! Cloning the main repository
+            var cloneMainProcess = new System.Diagnostics.Process();
+            cloneMainProcess.StartInfo.FileName = "git";
+            cloneMainProcess.StartInfo.Arguments = "clone https://github.com/prokenz101/utilities-cs-linux.git";
+            cloneMainProcess.StartInfo.CreateNoWindow = true;
+            cloneMainProcess.StartInfo.WorkingDirectory = UtilitiesFolderPath;
+            cloneMainProcess.Start();
+            cloneMainProcess.WaitForExit();
+            Console.WriteLine("\nCloned the repository.\n");
+
+            //! Building the cloned project into an executable
+            var buildClonedProject = new System.Diagnostics.Process();
+            buildClonedProject.StartInfo.FileName = "dotnet";
+            buildClonedProject.StartInfo.Arguments = "publish -c Release --self-contained false";
+            buildClonedProject.StartInfo.CreateNoWindow = true;
+            buildClonedProject.StartInfo.WorkingDirectory = Path.Combine(UtilitiesFolderPath, "utilities-cs-linux");
+            buildClonedProject.Start();
+            buildClonedProject.WaitForExit();
+            Console.WriteLine("\nBuilt the project.");
+
+            //! Move the obtained executable to the root utilities-cs folder
+            File.Move(
+                Path.Combine(UtilitiesFolderPath, "utilities-cs-linux/bin/Release/net6.0/linux-x64/publish/utilities-cs-linux"),
+                Path.Combine(UtilitiesFolderPath, "utilities-cs")
+            ); Console.WriteLine("\nMoved the obtained executable to root folder.");
+
+            //! Delete the cloned folder
+            Directory.Delete(Path.Combine(UtilitiesFolderPath, "utilities-cs-linux"), recursive: true);
+            Console.WriteLine("\nDeleted the cloned folder.");
         }
     }
 }
