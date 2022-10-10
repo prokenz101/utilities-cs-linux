9bfbff3f8f3cb7e2b03a605e989e050c24107688
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2022-10-10 21:23:03 +0400

    added main paths and URLs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿namespace utilities_cs_linux_setup {
     class Program {
+        public static string UtilitiesFolderPath = Path.Combine(Environment.GetEnvironmentVariable("HOME")!, "utilities-cs");
+        public static string IconPath = Path.Combine(UtilitiesFolderPath, "Assets/UtilitiesIcon.ico");
+        public static string IconURL = "https://raw.githubusercontent.com/prokenz101/utilities-cs-linux/setup/Assets/UtilitiesIcon.ico";
         static void Main(string[] args) {
             //* An entry point for the setup program.
         }
