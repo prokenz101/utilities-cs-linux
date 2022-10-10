8a1516aa217f80ba2d1c722fb0bb25ed73fb9f2f
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2022-10-10 21:23:29 +0400

    created main directory for all u-cs files

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
         public static string IconPath = Path.Combine(UtilitiesFolderPath, "Assets/UtilitiesIcon.ico");
         public static string IconURL = "https://raw.githubusercontent.com/prokenz101/utilities-cs-linux/setup/Assets/UtilitiesIcon.ico";
         static void Main(string[] args) {
+            Directory.CreateDirectory(Path.Combine(UtilitiesFolderPath, "Assets")); //* Creating folders
         }
     }
 }
