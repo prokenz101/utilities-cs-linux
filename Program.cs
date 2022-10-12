0a7af3f60fcec9310fa0b898449564720105245a
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2022-10-12 19:45:21 +0400

    made Home field

--- Program.cs
+++ Program.cs
@@ -1,10 +1,7 @@
 ﻿namespace utilities_cs_linux_setup {
     class Program {
-        public static string UtilitiesFolderPath = Path.Combine(
-            "/home",
-            System.Environment.UserName,
-            "utilities-cs"
-        );
+        public static string Home = Path.Combine("/home", System.Environment.UserName);
+        public static string UtilitiesFolderPath = Path.Combine(Home, "utilities-cs");
         public static string IconPath = Path.Combine(UtilitiesFolderPath, "Assets/UtilitiesIcon.ico");
         public static string IconURL = "https://raw.githubusercontent.com/prokenz101/utilities-cs-linux/setup/Assets/UtilitiesIcon.ico";
         public static string MainExecutablePath = Path.Combine(UtilitiesFolderPath, "utilities-cs");
