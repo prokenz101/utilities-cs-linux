1236bf6e1097a4b03d9f89928e943f3380c6fb66
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2022-11-18 20:33:18 +0400

    added indextesting

--- CSDependencies/Utils.cs
+++ CSDependencies/Utils.cs
@@ -20,6 +20,24 @@ namespace utilities_cs_linux {
 
             return string.Join("", converted);
         }
+
+        /// <summary>
+        /// Returns true if IndexTest failed, i.e there were no arguments other than the command.
+        /// Returns false if the program ran successfully with all arguments.
+        /// </summary>
+        /// <param name="args">All arguments passed when pressing main Ctrl+F8 function.</param>
+        /// <param name="argscount">The index that indextest will check to see if it exists.</param>
+        /// <returns>A bool that will be false if IndexTest failed, and true if it didn't.</returns>
+        public static string IndexTest(string[] args, int argscount = 1) {
+            try {
+                string test = args[argscount];
+                return "false";
+            } catch (IndexOutOfRangeException) {
+                return SocketJSON.SendJSON(
+                    "notification", new List<object>() { "Something went wrong.", "Check your parameters." }
+                );
+            }
+        }
     }
 
     public class SocketJSON {
