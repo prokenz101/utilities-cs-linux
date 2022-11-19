a2a6d39d3c7e470505fa6fa37814639467e0ad0d
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2022-11-19 20:11:49 +0400

    multiple changes
    moved SocketjSON class to sockets.py
    added command request mode for only the exit command (for now)

--- PythonDependencies/sockets.py
+++ PythonDependencies/sockets.py
@@ -1,9 +1,10 @@
 from time import sleep
+from json import loads
 from socket import socket
 from pynput import keyboard
 from pyperclip import paste
-from utils import SocketJSON
 from settings import Settings
+from utils import notification, copy
 
 ws = socket()
 port = 1234
@@ -19,6 +20,10 @@ def on_release(key):
                 sleep(Settings.dict["CopyingHotkeyDelay"] / 1000)
                 print(Settings.dict["CopyingHotkeyDelay"])
             with controller.pressed("c"):
+                pass
+
+        if Settings.dict["PressEscape"]:
+            with controller.pressed(keyboard.Key.esc):
                 pass
 
         send_receive_data(paste())
@@ -41,6 +46,44 @@ def send_receive_data(data):
 def main():
     with keyboard.Listener(on_release=on_release) as listener:
         listener.join()
+
+
+class SocketJSON:
+    def __init__(self, request: str, args: list):
+        self.request = request
+        self.args = args
+
+    @staticmethod
+    def read_json(json):
+        socket_response = loads(json)
+        return SocketJSON(socket_response["Request"], socket_response["Arguments"])
+
+    def process_data(self) -> None:
+        match self.request:
+            case "regular":
+                print(self.args[0])
+                copy(self.args[0])
+                notification(*self.args[1:])
+
+            case "notification":
+                notification(*self.args)
+
+            case "command":
+                match self.args[0]:
+                    case "exit":
+                        print("Exiting...")
+                        # ws.close()
+                        notification("Exiting.", "utilities-cs is exiting...")
+                        try:
+                            exit(0)
+                        except:
+                            pass
+
+                    case _:
+                        notification("Something went wrong.", "An exception occured.")
+
+            case _:
+                notification("Something went wrong.", "An exception occured.")
 
 
 if __name__ == "__main__":
--- PythonDependencies/utils.py
+++ PythonDependencies/utils.py
@@ -1,42 +1,14 @@
 import pyperclip
-from json import loads
+from pathlib import Path
 from pynput import keyboard
 from os import path, system
 from settings import Settings
 from posixpath import expanduser
-from pathlib import Path
 
 user = expanduser("~")
 utilities_cs_path = path.join(user, "utilities-cs")
 
 controller = keyboard.Controller()
-
-
-class SocketJSON:
-    def __init__(self, request: str, args: list):
-        self.request = request
-        self.args = args
-
-    @staticmethod
-    def read_json(json):
-        socket_response = loads(json)
-        return SocketJSON(socket_response["Request"], socket_response["Arguments"])
-
-    def process_data(self) -> None:
-        match self.request:
-            case "regular":
-                print(self.args[0])
-                copy(self.args[0])
-                notification(*self.args[1:])
-
-            case "notification":
-                notification(*self.args)
-                if Settings.dict["PressEscape"]:
-                    with controller.pressed(keyboard.Key.esc):
-                        pass
-            
-            case _:
-                print("not regular or notification")
 
 
 def notification(
@@ -73,7 +45,3 @@ def copy(str):
         with controller.pressed(keyboard.Key.ctrl):
             with controller.pressed("v"):
                 pass
-
-    if Settings.dict["PressEscape"]:
-        with controller.pressed(keyboard.Key.esc):
-            pass
