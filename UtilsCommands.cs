958891d40aea08c9068f7a3d35baefc1a3cc5bb1
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2022-11-22 11:51:05 +0400

    added pi

--- UtilsCommands.cs
+++ UtilsCommands.cs
@@ -952,6 +952,81 @@ namespace utilities_cs_linux {
                 allCommandMode: "fancy"
             );
 
+            FormattableCommand pi = new(
+                commandName: "pi",
+                function: (string[] args, bool copy, bool notif) => {
+                    string indexTest = Utils.IndexTest(args);
+                    if (indexTest != "false") { return indexTest; }
+
+                    try {
+                        int digits = int.Parse(args[1]);
+
+                        Func<System.Numerics.BigInteger, string> calculatePi = (System.Numerics.BigInteger digits) => {
+                            digits++;
+
+                            uint[] x = new uint[(int)digits * 10 / 3 + 2];
+                            uint[] r = new uint[(int)digits * 10 / 3 + 2];
+                            uint[] pi = new uint[(int)digits];
+
+                            for (int j = 0; j < x.Length; j++) { x[j] = 20; }
+
+                            for (int i = 0; i < digits; i++) {
+                                uint carry = 0;
+                                for (int j = 0; j < x.Length; j++) {
+                                    uint num = (uint)(x.Length - j - 1);
+                                    uint dem = num * 2 + 1;
+
+                                    x[j] += carry;
+
+                                    uint q = x[j] / dem;
+                                    r[j] = x[j] % dem;
+
+                                    carry = q * num;
+                                }
+
+
+                                pi[i] = (x[x.Length - 1] / 10);
+                                r[x.Length - 1] = x[x.Length - 1] % 10; ;
+                                for (int j = 0; j < x.Length; j++) { x[j] = r[j] * 10; }
+                            }
+
+                            var result = "";
+                            uint c = 0;
+
+                            for (int i = pi.Length - 1; i >= 0; i--) {
+                                pi[i] += c;
+                                c = pi[i] / 10;
+
+                                result = (pi[i] % 10).ToString() + result;
+                            }
+
+                            return result;
+                        };
+
+                        return digits <= 0 ? SocketJSON.SendJSON(
+                            "notification",
+                            new List<object>() { "Something went wrong.", "Invalid number of digits." }
+                        ) : digits <= 1000 ? Utils.CopyNotifCheck(
+                            copy, notif,
+                            new List<object>() { "3." + PiDigits.piDigits[0..digits], "Success!", "Check your clipboard." }
+                        ) : Utils.CopyNotifCheck(
+                            copy, notif,
+                            new List<object>() { "3." + calculatePi(digits)[1..], "Success!", "Check your clipboard." }
+                        );
+
+                    } catch (FormatException) {
+                        return SocketJSON.SendJSON(
+                            "notification", new List<object>() { "Something went wrong.", "Was that really a number?" }
+                        );
+                    } catch {
+                        return SocketJSON.SendJSON(
+                            "notification",
+                            new List<object>() { "Something went wrong.", "An exception has occured." }
+                        );
+                    }
+                }
+            );
+
             FormattableCommand mathitalic = new(
                 commandName: "mathitalic",
                 function: (string[] args, bool copy, bool notif) => {
