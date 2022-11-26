645d47d06cd662e4575d10e79175dd3d07f39819
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2022-11-26 14:38:36 +0400

    added sha256

--- UtilsCommands.cs
+++ UtilsCommands.cs
@@ -1826,6 +1826,30 @@ Word count: {args[1..].Length}";
                 }
             );
 
+            FormattableCommand sha256 = new(
+                commandName: "sha256",
+                function: (string[] args, bool copy, bool notif) => {
+                    string indexTest = Utils.IndexTest(args);
+                    if (indexTest != "false") { return indexTest; }
+
+                    string text = string.Join(" ", args[1..]);
+                    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+                    using (System.Security.Cryptography.SHA256 hash = System.Security.Cryptography.SHA256.Create()) {
+                        System.Text.Encoding enc = System.Text.Encoding.UTF8;
+                        Byte[] result = hash.ComputeHash(enc.GetBytes(text));
+
+                        foreach (Byte b in result)
+                            sb.Append(b.ToString("x2"));
+                    }
+
+                    return Utils.CopyNotifCheck(
+                        copy, notif,
+                        new List<object>() { sb.ToString(), "Success!", "Message copied to clipboard." }
+                    );
+                }
+            );
+
         }
     }
 }
